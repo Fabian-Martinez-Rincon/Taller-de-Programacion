53a2672a1a1db0b27a8b2e65e0a06122508fa2a0
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-08-26 17:33:50 -0300

    Ultimo

--- Teorias/Ejercicios/Pruebas.pas
+++ Teorias/Ejercicios/Pruebas.pas
@@ -32,22 +32,26 @@ begin
     end;
 end;
 //__________________________________________________________________________________
-Procedure Ordenar ( var v: vector; dimLog: Integer );
-
-var i, j, p: Integer; item : Integer;	
-		
+Procedure Ordenar ( var v: vector; dimL: Integer );
+var 
+    i, j, p: Integer; 
+    item : Integer;	
+    actual:integer;
 begin
-    for i:=1 to dimLog-1 do 
-    begin {busca el mínimo y guarda en p la posición}
-        p := i;
-        for j := i+1 to dimLog do
-            if v[ j ] < v[ p ] then p:=j;
-        {intercambia v[i] y v[p]}
-        item := v[ p ];   
-        v[ p ] := v[ i ];   
-        v[ i ] := item;
+    actual:=0;
+    for i:=2 to dimL do 
+    begin 
+        actual:= v[i];
+        j:= i-1; 
+        while (j > 0) and (v[j] > actual) do
+        begin
+            v[j+1]:= v[j];
+            j:=j-1;
+        end;  
+        v[j+1]:= actual; 
     end;
 end;
+
 
 //__________________________________________________________________________________
 var

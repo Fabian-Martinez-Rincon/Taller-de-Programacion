5bbc91f633a2a88efbef971c7f093e6d0bd54009
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-08-30 20:00:52 -0300

    empezando 2

--- Practicas/Practica 1/1.pas
+++ Practicas/Practica 1/1.pas
@@ -1,3 +1,10 @@
+{
+1. El administrador de un edificio de oficinas cuenta, en papel, con la información del pago de las expensas de dichas oficinas. Implementar un programa modularizado que:
+
+a. Genere un vector, sin orden, con a lo sumo las 300 oficinas que administra. De cada oficina se ingresa el código de identificación, DNI del propietario y valor de la expensa. La lectura finaliza cuando se ingresa el código de identificación -1, el cual no se procesa.
+b. Ordene el vector, aplicando el método de inserción, por código de identificación de la oficina.
+c. Ordene el vector aplicando el método de selección, por código de identificación de la oficina.
+}
 program uno;
 const
     dimF = 300;
---
+++ Practicas/Practica 1/2.pas
@@ -0,0 +1,69 @@
+{
+2. Netflix ha publicado la lista de películas que estarán disponibles durante el mes de diciembre de 2021. De cada película se conoce: código de película, código de género (1: acción, 2: aventura, 3: drama, 4: suspenso, 5: comedia, 6: bélica, 7: documental y 8: terror)
+y puntaje promedio otorgado por las críticas.
+Implementar un programa modularizado que:
+a. Lea los datos de películas y los almacene por orden de llegada y agrupados por código de género, en una estructura de datos adecuada. La lectura finaliza cuando se lee el código de la película -1.
+b. Una vez almacenada la información, genere un vector que guarde, para cada género, el código de película con mayor puntaje obtenido entre todas las críticas.
+c. Ordene los elementos del vector generado en b) por puntaje utilizando alguno de los dos métodos vistos en la teoría.
+d. Luego de ordenar el vector, muestre el código de película con mayor puntaje y el código de película con menor puntaje
+}
+program dos;
+type
+    rango = 1..8;
+    pelicula = record
+        cod_Peli:integer;
+        cod_genero:rango;
+        punt_prom:real;
+    end;
+    lista = ^nodo;
+    nodo = record
+        dato:pelicula;
+        sig:lista;
+    end;
+//_________________________________________________________________
+procedure AgregarAlFinal1(var pri:lista;per:pelicula); 
+var  
+   act, nue : lista;
+begin 
+   new (nue);
+   nue^.datos:= per;
+   nue^.sig := NIL;
+   if pri <> Nil then 
+   begin
+       act := pri ;
+       while  (act^.sig <> NIL ) do 
+           act := act^.sig ;
+       act^.sig := nue ;
+   end
+   else
+       pri:= nue;
+end;
+//_________________________________________________________________
+procedure leerPeli(var p:pelicula);
+begin
+    writeln('Codigo de Pelicula: '); ReadLn(p.cod_Peli);
+    if (p.cod_Peli <> -1) then
+    begin
+        writeln('Codigo de Genero: '); ReadLn(p.cod_genero);
+        writeln('Puntaje Promedio: '); ReadLn(p.punt_prom);
+    end;
+    
+end;
+//_________________________________________________________________
+procedure CargarPelis(var l:lista);
+var
+    p:pelicula;
+begin
+    leerPeli(p);
+    while  do
+    begin
+        leerPeli(p);
+    end;
+        
+end;
+//_________________________________________________________________
+var
+    l:lista;
+begin
+    CargarPelis(l);
+end.

6121aab92b8c0e450d3081283a4f5aaf61f21303
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-02 03:27:40 -0300

    arboles

---
+++ Practicas/Practica 1/4.pas
@@ -0,0 +1,8 @@
+{
+4. Escribir un programa que:
+a. Implemente un módulo que genere una lista a partir de la lectura de números
+enteros. La lectura finaliza con el número -1.
+b. Implemente un módulo recursivo que devuelva el mínimo valor de la lista.
+c. Implemente un módulo recursivo que devuelva el máximo valor de la lista.
+d. Implemente un módulo recursivo que devuelva verdadero si un valor determinado
+se encuentra en la lista o falso en caso contrario.}

119ae7b5889d8ea6d5a0ebf720b56e120ad04fca
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-02 03:46:51 -0300

    4tro

--- Practicas/Practica 1/4.pas
+++ Practicas/Practica 1/4.pas
@@ -6,3 +6,12 @@ b. Implemente un módulo recursivo que devuelva el mínimo valor de la lista.
 c. Implemente un módulo recursivo que devuelva el máximo valor de la lista.
 d. Implemente un módulo recursivo que devuelva verdadero si un valor determinado
 se encuentra en la lista o falso en caso contrario.}
+Procedure imprimir (L:lista);
+Begin
+    if (L <> nil) then
+    begin
+        write (L^.dato);
+        L:= L^.sig;
+        imprimir (L);
+    end;
+End;

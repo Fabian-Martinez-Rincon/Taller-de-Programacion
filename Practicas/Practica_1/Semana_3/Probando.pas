eab04a3ca4ddaeb4934ae33b980db8b6c46841a7
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-19 22:00:56 -0300

    codigo

--- Practicas/Practica_1/Semana_3/Probando.pas
+++ Practicas/Practica_1/Semana_3/Probando.pas
@@ -1,26 +1,145 @@
+{11. Un cine posee la lista de películas que proyectará durante el mes de octubre. De cada
+película se conoce: código de película, código de género (1: acción, 2: aventura, 3: drama,
+4: suspenso, 5: comedia, 6: bélica, 7: documental y 8: terror) y puntaje promedio otorgado
+por las críticas.
+Implementar un programa que contenga:
+a. Un módulo que lea los datos de películas y los almacene ordenados por código de
+película y agrupados por código de género, en una estructura de datos adecuada. La
+lectura finaliza cuando se lee el código de película -1.
+b. Un módulo que reciba la estructura generada en el punto a y retorne una
+estructura de datos donde estén todas las películas almacenadas ordenadas por código de
+película.
+}
+program Fabian_once;   
 const
-    N = 10;
+    dimF = 8;
+    cantidad_pelis = 20;
 type
-      Datos = record
-           dato_ordenamiento: string;
-           dato1: integer;
-           dato2: real;
-      end; 
-      LISTA = ^nodo;
-      nodo = record
-           data: Datos;
-           sig: LISTA;
-      end;
-      VECTOR_LISTAS = array[1..N] of LISTA;
-      Datos2 = record
-           dato_ordenamiento: string;
-           sumaDato1: integer;
-      end;
-      LISTA2 = ^nodo;
-      nodo = record
-           data: Datos2;
-           sig: LISTA2;
-      end; 
+    rango = 1..dimF;
+    peliculas = record
+        codigo:integer;
+        genero:rango;
+        puntaje_Promedio:real;
+    end;
+    vector_pelis = array [1..cantidad_pelis] of peliculas;
+
+    vector_dimL = array [rango] of integer; //Para el vector, por unidad
+
+    vector = array [rango] of vector_pelis;
+//___________________________________________________________________________
+procedure leerPelicula(var p:peliculas);
+begin
+    p.codigo:=-1+random(20);  //el if que chequea corte de control
+    p.genero:=random(10);
+    p.puntaje_Promedio:=random(10);
+end;
+//___________________________________________________________________________
+Procedure Insertar (var v:vector_pelis; var dimL:integer; pos: integer; elem:peliculas);
+var 
+    i: integer;
+begin
+    for i:= dimL downto pos do 
+        v [i + 1] := v [i] ;
+    v[pos]:= elem; 
+    dimL := dimL + 1;
+End;
+//__________________________________________________________________________________
+Function BuscarPosicion (x:integer;v:vector_pelis;dimL:integer):integer;
 var
-    listas_entrada: VECTOR_LISTAS;
-    lista_de_salida: LISTA2;
+    pos:Integer;
+begin
+    pos:=1; 
+    while (pos <= dimL) and (x > v[pos].codigo) do
+        pos:=pos + 1;
+    BuscarPosicion:=pos;
+end;
+//___________________________________________________________________________
+procedure Manteniendo_un_Orden(var v:vector_pelis; var dimL:integer;elemento:peliculas);
+var
+    pos:integer;
+begin
+    if (dimL < cantidad_pelis) then
+    begin
+        pos:=BuscarPosicion(elemento.codigo,v,dimL);
+        Insertar(v,dimL,pos,elemento);
+    end;
+end;
+//__________________________________________________________________________________
+procedure Cargar_Peliculas(var v:vector;var v_dimL:vector_dimL);
+var
+    p:peliculas;
+begin
+    leerPelicula(p);
+    while (p.codigo <> -1) and (v_dimL[p.genero] < cantidad_pelis) do //Como mucho 20
+    begin
+        Manteniendo_un_Orden(v[p.genero],v_dimL[p.genero],p);
+        leerPelicula(p);
+    end;
+end;
+//___________________________________________________________________________
+{procedure minimo(var v : vector; var min : peliculas);
+var
+  i, posMin : integer;
+begin
+	min.codigo := 9999;
+	posMin := -1;
+	
+	for i := 1 to dimF do 
+		if (v[i] <> NIL) and (v[i]^.dato.codigo <= min.codigo)  then 
+		begin
+			min.codigo := v[i]^.dato.codigo; 
+            posMin := i;		
+		end;
+	if (posMin <> -1) then
+	begin                
+        min.genero:=v[posMin]^.dato.genero;
+        min.puntaje_Promedio:=v[posMin]^.dato.puntaje_Promedio;
+		v[posMin] := v[posMin]^.sig; 
+	end
+end;
+//___________________________________________________________________________
+procedure merge(v : vector; var l_nueva:lista);
+var
+	ult : lista;
+	min : peliculas;
+begin
+	minimo(v,min);	
+	while (min.codigo <> 9999) do 
+	begin
+        AgregarAlFinal2(ult,l_nueva,min);
+        minimo(v,min);
+	end;
+end;}
+//___________________________________________________________________________
+procedure Inicializar_Peliculas(var v:vector_dimL);
+var 
+    i:Integer;
+begin
+    for i:=1 to dimF do
+    begin
+        v[i]:=0;
+    end;
+end;
+//___________________________________________________________________________
+procedure Imprimir_DimF(v_dimL:vector_dimL);
+var
+    i:integer;
+begin
+    for i:=1 to dimF do
+    begin
+        WriteLn('Cosa: ',v_dimL[i]);
+    end; 
+end;
+//___________________________________________________________________________
+var
+    v:vector;
+    v_dimL:vector_dimL;
+begin
+    randomize;
+    WriteLn('Hola');
+    Inicializar_Peliculas(v_dimL);
+    Cargar_Peliculas(v,v_dimL); //A
+    WriteLn('Dimensiones Logicas');
+    Imprimir_DimF(v_dimL);
+    //merge(v,l_nueva); //B
+end.
---
+++ Practicas/Practica_1/Semana_3/Probando2.pas
@@ -0,0 +1,124 @@
+program Fabian_once;   
+const
+    dimF = 8;
+    cantidad_pelis = 20;
+type
+    rango = 1..dimF;
+    peliculas = record
+        codigo:integer;
+        genero:rango;
+        puntaje_Promedio:real;
+    end;
+    vector_pelis = array [1..cantidad_pelis] of peliculas;
+
+    vector_dimL = array [rango] of integer; //Para el vector, por unidad
+
+    vector = array [rango] of vector_pelis;
+//____________________________________________________________________________________
+Procedure Insertar (var v:vector_pelis; var dimL:integer; pos: integer; elem:peliculas);
+var 
+    i: integer;
+begin
+    for i:= dimL downto pos do 
+        v [i + 1] := v [i] ;
+    v[pos]:= elem; 
+    dimL := dimL + 1;
+End;
+//__________________________________________________________________________________
+Function BuscarPosicion (x:integer;v:vector_pelis;dimL:integer):integer;
+var
+    pos:Integer;
+begin
+    pos:=1; 
+    while (pos <= cantidad_pelis) and (x > v[pos].codigo) do
+        pos:=pos + 1;
+    BuscarPosicion:=pos;
+end;
+//___________________________________________________________________________
+procedure Manteniendo_un_Orden(var v:vector_pelis; var dimL:integer;elemento:peliculas);
+var
+    pos:integer;
+begin
+    if (dimL < cantidad_pelis) then
+    begin
+        pos:=BuscarPosicion(elemento.codigo,v,dimL);
+        Insertar(v,dimL,pos,elemento);
+    end;
+end;
+//____________________________________________________________________________________
+procedure leerPelicula(var p:peliculas);
+begin
+    p.codigo:=-1+random(20);  //el if que chequea corte de control
+    WriteLn('Codigo: ', p.codigo);
+    p.genero:=1+random(7);
+    WriteLn('Genero: ', p.genero);
+    p.puntaje_Promedio:=random(10);
+    WriteLn('Puntaje: ', p.puntaje_Promedio);
+    Writeln('_____________________');
+end;
+//____________________________________________________________________________________
+procedure Cargar_Peliculas(var v:vector;var v_dimL:vector_dimL);
+var
+    p:peliculas;
+begin
+    leerPelicula(p);
+    while (p.codigo <> -1) and (v_dimL[p.genero] < cantidad_pelis) do //Como mucho 20
+    begin
+        Manteniendo_un_Orden(v[p.genero],v_dimL[p.genero],p);
+        leerPelicula(p);
+    end;
+end;
+//____________________________________________________________________________________
+procedure ImprimirVector(v:vector;dimL:vector_dimL);
+var
+    i:integer;
+    j:integer;
+begin
+    for j:=1 to dimF do
+    begin
+        WriteLn('Genero: ',i);
+        WriteLn('Dimension Logica: ', dimL[j]);
+        for i:=1 to dimL[j] do
+        begin
+            writeln('Codigo: ', (v[j])[i].codigo);
+            writeln('Genero: ', (v[j])[i].genero);
+            writeln('Puntaje: ', (v[j])[i].puntaje_Promedio:2:2);
+        end;
+        WriteLn('____________________________________');
+    end;
+end;
+//____________________________________________________________________________________
+procedure Inicializar_Peliculas(var v:vector_dimL);
+var 
+    i:Integer;
+begin
+    for i:=1 to dimF do
+    begin
+        v[i]:=0;
+    end;
+end;
+//____________________________________________________________________________________
+procedure Imprimir_DimF(dimL:vector_dimL);
+var
+    i:integer;
+    j:integer;
+begin
+    for i:=1 to dimF do
+    begin
+        WriteLn('Genero: ',i);
+        WriteLn('DimL: ', (dimL[i]));
+    end;
+end;
+//____________________________________________________________________________________
+var
+    v:vector;
+    v_dimL:vector_dimL;
+begin
+    randomize;
+    WriteLn('Hola');
+    Inicializar_Peliculas(v_dimL);
+    Cargar_Peliculas(v,v_dimL); //A
+    ImprimirVector(v,v_dimL);
+    //WriteLn('Dimensiones Logicas');
+    //Imprimir_DimF(v_dimL);
+end.

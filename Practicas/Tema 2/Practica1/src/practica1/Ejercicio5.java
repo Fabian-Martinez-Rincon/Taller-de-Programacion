323904cd65ecd2fe1d99821ca41eb38033beb636
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-22 22:32:07 -0300

    ejercicio 5

--- Practicas/Tema 2/Practica1/src/practica1/Ejercicio5.java
+++ Practicas/Tema 2/Practica1/src/practica1/Ejercicio5.java
@@ -9,6 +9,34 @@ finalizar la llegada de personas, informe lo pedido.
 package practica1;
 
 
+import PaqueteLectura.GeneradorAleatorio;
+
 public class Ejercicio5 {
-    
+    public static void main(String[] args) {
+        GeneradorAleatorio.iniciar();
+        
+        int x=8,y = 4;
+        int[][] edificio = new int[x][y];
+	
+        int nro_piso = GeneradorAleatorio.generarInt(9);
+        System.out.println("Piso: "+ nro_piso);
+        int nro_oficina = GeneradorAleatorio.generarInt(4);
+        System.out.println("Oficina: "+ nro_oficina);
+        
+        while (nro_piso != 8) {
+            edificio[nro_piso][nro_oficina]++;
+            nro_piso = GeneradorAleatorio.generarInt(9);
+            System.out.println("Piso: "+ nro_piso);
+            nro_oficina = GeneradorAleatorio.generarInt(4);
+            System.out.println("Oficina: "+ nro_oficina);
+        }        
+        
+        
+        for (int i=0;i<x;i++){  //Imprimir Matriz
+            for(int j=0;j<y;j++)
+                System.out.print(edificio[i][j] + " ");
+            System.out.println();
+        }
+            
+    }
 }
---
+++ Practicas/Tema 2/Practica1/src/practica1/Ejercicio6.java
@@ -0,0 +1,19 @@
+/*
+6- Escriba un programa que simule el ingreso de personas a un banco. Cada 
+persona que ingresa indica la operación que desea realizar (0: “cobro de cheque” 1: 
+“depósito/ extracción en cuenta” 2: “pago de impuestos o servicios” 3: “cobro de 
+jubilación” 4: “cobro de planes”). La recepción de personas culmina cuando un 
+empleado ingresa la operación 5 (cierre del banco). Informar la cantidad de 
+personas atendidas por cada operación y la operación más solicitada. 
+ */
+package practica1;
+
+/**
+ *
+ * @author fabia
+ */
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}

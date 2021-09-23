2fd2b35ceef3cec7266d8c9f72654a1eb37f6831
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-22 22:59:49 -0300

    ejercicioSeis

--- Practicas/Tema 2/Practica1/src/practica1/Ejercicio6.java
+++ Practicas/Tema 2/Practica1/src/practica1/Ejercicio6.java
@@ -8,12 +8,42 @@ personas atendidas por cada operación y la operación más solicitada.
  */
 package practica1;
 
-/**
- *
- * @author fabia
- */
+import PaqueteLectura.GeneradorAleatorio;
 public class Ejercicio6 {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        GeneradorAleatorio.iniciar();
+        int nro;
+        int [] contador = new int[4];
+        boolean cumple = false;
+        while (cumple == false) {  
+            nro = 1+(GeneradorAleatorio.generarInt(5));
+            System.out.println("nro: "+ nro);
+            switch (nro) 
+            {
+            case 1:   System.out.println("Cobro de cheque"); contador[0]++;
+                     break;
+            case 2:   System.out.println("Deposito / Extraccion de cuenta");contador[1]++;
+                     break;
+            case 3:   System.out.println("Pago de impuestos o servicios");contador[2]++;
+                     break;
+            case 4:   System.out.println("Cobro de jubilacion");contador[3]++;
+                     break;
+            case 5:   cumple = true;
+                     break;
+            default:  System.out.println("Numero invalida ");
+                     break;
+            }            
+        }
+        int maxima = -1;
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Vector: "+i+" Cantidad: " + contador[i]);
+            if (contador[i]>maxima){
+                maxima = i;
+            }
+        }
+        maxima++; //Pongo esto, para que sea acorde a los numeros impuestos por el enunciado :D
+        System.out.println("La opcion mas elegida es : "+ maxima);
+        
+    }
     }
 }

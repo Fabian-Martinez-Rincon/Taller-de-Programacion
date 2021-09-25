1d08b7b54227d0af4559ae41fddd2ef1a90c8863
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-25 20:25:49 -0300

    Ejercicio 4 corregido

--- Practicas/Tema 2/Practica1/src/practica1/Ejercicio4_Parte2.java
+++ Practicas/Tema 2/Practica1/src/practica1/Ejercicio4_Parte2.java
@@ -8,19 +8,36 @@ siguiendo. La inscripción finaliza al llegar una persona con nombre “ZZZ” o
 40 cupos de casting.
 Una vez finalizada la inscripción: 
 b) Informar para cada día y turno el nombre de la persona a entrevistar.
-NOTA: utilizar la clase Persona y pensar en la estructura de datos a utilizar. 
+NOTA: utilizar la clase Persona y pensar en la estructura de datos a utilizar.
  */
 package practica1;
+import PaqueteLectura.GeneradorAleatorio;
 
 public class Ejercicio4_Parte2 {
     public static void main(String[] args) {
-        int x=5,y = 10;
-        Persona programa[][]=new Persona[x][y];
-        Persona persona1 = new Persona();
-        persona1.nombre = 
+        GeneradorAleatorio.iniciar();
+        int dias= 5, entrevistas= 8;
+        String nom;
+        Persona [][] inscripciones= new Persona[dias][entrevistas];
+        int i= 0,j;
+        nom= GeneradorAleatorio.generarString(5);
         
-        while (!= "ZZZ") {            
-            
+        while (!nom.equals("ZZZ")&&(i<dias)) {
+            j = 0;
+            while (!nom.equals("ZZZ")&&(j<entrevistas)) {
+                inscripciones[i][j]= new Persona();
+                inscripciones[i][j].setNombre(nom);
+                inscripciones[i][j].setDNI(GeneradorAleatorio.generarInt(10));
+                inscripciones[i][j].setEdad(GeneradorAleatorio.generarInt(10));
+                j++;
+            }
+            i++;
+            nom= GeneradorAleatorio.generarString(5);
+        }
+        for (i=0;i<dias;i++){  //Imprimir Matriz
+            System.out.println("Semana : " + (i+1));
+            for(j=0;j<entrevistas;j++)
+                System.out.println("Para el dia "+ (j+1) +" de la semana "+ (i+1)+" se entevista a "+inscripciones[i][j].getNombre() ); 
         }
     }
 }

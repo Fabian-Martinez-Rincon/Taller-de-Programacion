0546d21b5a9877fc643256c88d4ee62ec41cf4df
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-10-08 15:01:16 -0300

    Java codigo

---
+++ Practicas/Tema 2/Practica2/src/practica2/Probando.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Libro {
+   /* Declaración del estado */
+   private String titulo;
+   private String primerAutor; 
+   private String editorial;
+   private int añoEdicion;
+   private String ISBN; 
+   private double precio;  
+}

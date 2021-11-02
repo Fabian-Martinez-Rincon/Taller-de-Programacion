67f435d7b11f3a82649275c5b8b12e99fe24f068
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-11-02 12:51:40 -0300

    ejercicios

---
+++ Java/Practica3Pro/practica3Pro/build/classes/practica3pro/Figura.rs
@@ -0,0 +1,1 @@
+tema5.Figura
---
+++ Java/Practica3Pro/practica3Pro/src/practica3pro/Practica3Pro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3pro;
+
+/**
+ *
+ * @author fabia
+ */
+public class Practica3Pro {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
